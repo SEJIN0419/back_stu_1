ae3d631b230e2d4987194a50d81a15d588224eee
SEJIN0419 <ksg4019113@mail.ulsan.ac.kr>
2022-12-15 22:13:27 +0900

    second test commit

--- backj_stu1.py
+++ backj_stu1.py
@@ -1,4 +1,13 @@
-print("hello")
+while(True):
+    print("온도를 입력하세요(100을 입력하면 종료합니다.): ",end='')
+    temp = int(input())
+    if temp == 100:
+        print("온도 알림을 종료합니다.")
+        break
+    elif temp < 5:
+        print("오늘은 매우 춥습니다.")
+    else:
+        print("따뜻합니다!")
     
 
 
